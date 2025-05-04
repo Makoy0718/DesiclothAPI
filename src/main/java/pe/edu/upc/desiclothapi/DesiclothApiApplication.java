0e969e9c5636ef480e1c9dcfa078e59945e8b3d2
Makoy0718 <marconm04@hotmail.com>
2025-05-04 00:51:26 -0500

    main

---
+++ src/main/java/pe/edu/upc/desiclothapi/DesiclothApiApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.desiclothapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesiclothApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesiclothApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/desiclothapi/DesiclothApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.desiclothapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesiclothApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
