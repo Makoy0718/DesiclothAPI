5eea9d0b4c01087397005b3f94eca6b044a24231
Brilrs <bricm304@gmail.com>
2025-05-07 01:08:35 -0500

    HU-PED-11

---
+++ src/main/java/pe/edu/upc/desiclothapi/repositories/IPedidoRepository.java
@@ -0,0 +1,19 @@
+package pe.edu.upc.desiclothapi.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.desiclothapi.entities.Pedido;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface IPedidoRepository extends JpaRepository<Pedido, Integer> {
+
+    @Query("select p from Pedido p where p.fechaPedido = :fecha")
+    List<Pedido> buscarPorFechaPedido(@Param("fecha") LocalDate fecha);
+
+}
---
+++ src/main/java/pe/edu/upc/desiclothapi/serviciesinterfaces/IPedidoService.java
@@ -0,0 +1,11 @@
+package pe.edu.upc.desiclothapi.serviciesinterfaces;
+
+import pe.edu.upc.desiclothapi.entities.Pedido;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface IPedidoService {
+    public List<Pedido> listPedido();
+    List<Pedido> buscarPorFechaPedido(LocalDate fecha);
+}
