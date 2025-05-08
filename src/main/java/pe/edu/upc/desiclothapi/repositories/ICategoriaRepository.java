9087f74d3d2a4f66c963749fe4afdfab3924f2f2
Makoy0718 <marconm04@hotmail.com>
2025-05-07 20:33:37 -0500

    HU-CAT-30 Buscar una categoria

--- src/main/java/pe/edu/upc/desiclothapi/repositories/ICategoriaRepository.java
+++ src/main/java/pe/edu/upc/desiclothapi/repositories/ICategoriaRepository.java
@@ -2,6 +2,7 @@ package pe.edu.upc.desiclothapi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pe.edu.upc.desiclothapi.entities.Categoria;
 
 import java.util.List;
@@ -13,4 +14,7 @@ public interface ICategoriaRepository extends JpaRepository<Categoria, Integer> 
     //HU-CAT-34-02
     @Query("SELECT c FROM Categoria c ORDER BY c.nombreCategoria DESC")
     List<Categoria> ordenarZA();
+    //HU-CAT-30
+    @Query("select a from Categoria a where a.nombreCategoria like %:nombreC%")
+    public List<Categoria> buscarCategoria(@Param("nombreC") String nombreC);
 }
