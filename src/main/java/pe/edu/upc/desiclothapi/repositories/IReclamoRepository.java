83983554cea7f5f056e8418456e38d82b2671330
Alejandro <zamoraaleajndro221@gmail.com>
2025-05-07 23:55:26 -0500

    Realice: HU-REC-06-BUSCAR-MIS-RECLAMOS

--- src/main/java/pe/edu/upc/desiclothapi/repositories/IReclamoRepository.java
+++ src/main/java/pe/edu/upc/desiclothapi/repositories/IReclamoRepository.java
@@ -1,9 +1,18 @@
 package pe.edu.upc.desiclothapi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.desiclothapi.entities.Reclamo;
 
+import java.util.List;
+
+
 @Repository
 public interface IReclamoRepository extends JpaRepository<Reclamo, Integer> {
+
+    //HU-REC-06
+    @Query("select r from Reclamo r where lower(r.titulo) like lower(concat('%', :titulo, '%'))")
+    List<Reclamo> buscarPorTitulo(@Param("titulo") String titulo);
 }
