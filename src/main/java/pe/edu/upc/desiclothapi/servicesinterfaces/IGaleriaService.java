f69edfd2545aa00c03163b84afc0caa5dddb15ca
Alejandro <zamoraaleajndro221@gmail.com>
2025-05-10 22:58:15 -0500

    Realice: buscar_por_id_galeria

--- src/main/java/pe/edu/upc/desiclothapi/servicesinterfaces/IGaleriaService.java
+++ src/main/java/pe/edu/upc/desiclothapi/servicesinterfaces/IGaleriaService.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface IGaleriaService {
     public List<Galeria> listGaleria(); //HU-GAL--45
     public void insertGaleria(Galeria g); //HU-GAL--41
+    public Galeria buscarGaleriaPorId(int id); //buscar-id-galeria
     public List<Galeria> searchByNombre(String nombre);//HU-GAL--42
     public Galeria cambiarVisibilidadGaleria(int idGaleria, boolean visibilidad);//HU-GAL--43
     public void valorarGaleria(int idGaleria, int rating);//HU-GAL--44
